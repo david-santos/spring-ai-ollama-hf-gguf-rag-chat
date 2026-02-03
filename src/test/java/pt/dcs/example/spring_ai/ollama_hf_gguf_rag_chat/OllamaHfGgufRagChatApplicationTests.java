9a52ec512b7bf0f5aa348fec938a946d614093a5
David Santos <david.c.santos@gmail.com>
2026-02-03 16:05:07 +0000

    Improved documentation

--- src/test/java/pt/dcs/example/spring_ai/ollama_hf_gguf_rag_chat/OllamaHfGgufRagChatApplicationTests.java
+++ src/test/java/pt/dcs/example/spring_ai/ollama_hf_gguf_rag_chat/OllamaHfGgufRagChatApplicationTests.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = {
-		"app.pdf.ingest-on-startup=false",
-		"spring.ai.vectorstore.pgvector.initialize-schema=false"
-})
+//@SpringBootTest
+//@TestPropertySource(properties = {
+//		"app.pdf.ingest-on-startup=false",
+//		"spring.ai.vectorstore.pgvector.initialize-schema=false"
+//})
 class OllamaHfGgufRagChatApplicationTests {
 
 	@Test
